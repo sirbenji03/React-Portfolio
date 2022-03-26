e162a0b5be7b1b5cdf9ea287db03846efb37d00e
benjamin amakye <bamakye20@gmail.com>
2022-03-26 14:18:16 -0400

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
-
+import Navbar from "./components/Navbar";
 function App() {
   return (
-   <h2>portfolio</h2>
+   <Navbar/>
   );
 }
 
