47db699228874b8abad75176ef2b418173839cd2
benjamin amakye <bamakye20@gmail.com>
2022-03-27 01:54:31 -0400

    experience completed

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import AboutMe from "./components/AboutMe";
 import Services from "./components/Services";
+import Experience from "./components/Experience";
 
 
 function App() {
@@ -28,6 +29,7 @@ function App() {
     <Header/>
     <AboutMe/>
     <Services/>
+    <Experience/>
    </>
   );
 }
